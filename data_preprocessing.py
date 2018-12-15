f65a8ab8f061d8b9c15f9ed9b0b5507cc71c56d8
ManarArabi <manar.araby.ma@gmail.com>
2018-12-15 12:12:14 +0200

    methods are updated

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,3 +1,10 @@
+import pandas as pd
+
+import numpy as np
+
+import re
+
+
 class preprocessing(object):
 
     train = None
@@ -7,20 +14,39 @@ class preprocessing(object):
         #constructor
         return
 
-    def clean_tweets(self, data):
-        cleaned_tweets = []
+    def remove_pattern(self, text, pattern):
+        patterns = re.findall(pattern, text)
+        for i in patterns:
+            text = re.sub(i, '', text)
 
+        return text
+
+    def clean_tweets(self, data):
+        temp = [None] * len(data['tweet'])
+
+        for i in range(len(data['tweet'])):
+            temp[i] = self.remove_pattern(data['tweet'][i], "@[\w]*")
+            temp[i] = temp[i].replace("[^a-zA-Z#]", " ")
+            line_string = ' '
+            temp[i] = line_string.join([w for w in temp[i].split() if len(w) > 3])
+
+        data['tidy_tweet'] = temp
         print("The tweets is cleaned.")
-        return cleaned_tweets
+        return data
+
+    def make_combination(self, train, test):
+        combi = train.append(test, ignore_index=True)
+        return combi
 
     def read_data(self):
 
-        self.train = []
-        self.test = []
-        self.combi = [] #combination of training and testing data
+        self.train = pd.read_csv('train_E6oV3lV.csv')
+        self.test = pd.read_csv('test_tweets_anuFYb8.csv')
+        self.combi = self.make_combination(self.train, self.test)
         print("The data is read.")
         return self.train, self.test, self.combi
 
-    def visualize_data(self):
+    def visualize_data(self, data):
+        data.head()
         return
 
